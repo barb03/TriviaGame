518e35a48301156fc1133ba558ff2cdd808a2ddf
Vader55 <38086201+barb03@users.noreply.github.com>
2018-10-31 18:33:58 -0500

    added trivia questions to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+var questions = [{
+    ques: "Which NASCAR driver is known for this fact 'endured the loss of his father in the Daytona 500'",
+    ans: ["Kobe Bryant", "Dale Earnhardt Jr.", "Tony Gwynn", "Jim Palmer"],   
+    correct: "Dale Earnhardt Jr.",    
+},
+{
+    ques: "Who won the Inaugural Daytona 500?",
+    ans: ["Lee Petty", "Curtis Turner", "Richard Petty", "Fireball Roberts"],    
+    correct: "Lee Petty",    
+},
+{
+    ques: "What was the nikename for the four engine B-17 bomber planes used during WWII?",
+    ans: ["Tiger", "Jolly Roger", "Flying Fortress", "Puking Dog"],    
+    correct: "Flying Fortress",    
+},
+{
+    ques: "A Boeing 777 is equipped with how many engines?",
+    ans: ["4", "2", "3", "5"],    
+    correct: "2",   
+},
+{
+    ques: "In what year did aviator Charles A. Lindbergh cross the Atlantic Ocean?",
+    ans: ["1927", "1932", "1933", "1940"],    
+    correct: "1927",    
+},
+{
+    ques: "What automobile manufacturer was the first to implement the assembly line for the mass production of an entire automoble?",
+    ans: ["Ford", "Chevy", "Dodge", "Toyota"],    
+    correct: "Ford",   
+},
+{
+    ques: "What is the only bird known to fly backwards?",
+    ans: ["vulture", "starling", "hummingbird", "blue jay"],    
+    correct: "hummingbird",    
+},
+{
+    ques: "The term wake, kettle, or committee refers to a group of what bird?",
+    ans: ["vulture", "starling", "hummingbird", "blue jay"],    
+    correct: "vulture",    
+},
+{
+    ques: "What is the proper term for a group of parrots?",
+    ans: ["pandemonium", "parliment", "cygnet", "murder"],    
+    correct: "pandemonium",    
+},
+{
+    ques: "What is a flock of crows called?",
+    ans: ["pandemonium", "parliment", "cygnet", "murder"],    
+    correct: "murder",   
+}
+] // end questions object
